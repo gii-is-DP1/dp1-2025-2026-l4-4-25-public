6aef9d7df54adf5720bf15b04e855c0e19cbd9c2
GBK4935 <marayabla@alum.us.es>
2025-10-05 01:37:18 +0200

    Comentario en el formulario del "Login" para que la aplicación funcione hasta implementación del backend

--- frontend/src/auth/login/form/loginFormInputs.js
+++ frontend/src/auth/login/form/loginFormInputs.js
@@ -1,4 +1,4 @@
-import { formValidators } from "../../../validators/formValidators";
+/* import { formValidators } from "../../../validators/formValidators";
 
 export const loginFormInputs = [
   {
@@ -12,6 +12,27 @@ export const loginFormInputs = [
   {
     tag: "Password",
     name: "contrasena",
+    type: "password",
+    defaultValue: "",
+    isRequired: true,
+    validators: [formValidators.notEmptyValidator],
+  },
+]; */ // PARTE DE CARLOS HAY QUE ESPERAR AL BACKEND ---> MIENTRAS TANTO USAR NOMBRE Y CONTRASEÑA QUE USAMOS EN LAB
+
+import { formValidators } from "../../../validators/formValidators";
+
+export const loginFormInputs = [
+  {
+    tag: "Username",
+    name: "username",
+    type: "text",
+    defaultValue: "",
+    isRequired: true,
+    validators: [formValidators.notEmptyValidator],
+  },
+  {
+    tag: "Password",
+    name: "password",
     type: "password",
     defaultValue: "",
     isRequired: true,
