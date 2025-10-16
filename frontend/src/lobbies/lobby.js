2f57a5c6ab0878348d45dd1b9a33861a00adb259
Luiscc6 <luisclca6@gmail.com>
2025-10-16 21:56:34 +0200

    Refactor CreateGame and Lobby components; implement chat handling and game creation logic, and add generateRandomLink utility function.

--- frontend/src/lobbies/lobby.js
+++ frontend/src/lobbies/lobby.js
@@ -1,43 +1,155 @@
-import React from 'react';
+import React, { useState,useEffect } from 'react';
 import '../App.css';
 import '../static/css/home/home.css'; 
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import tokenService from '../services/token.service';
+import generateRandomLink from '../util/generateRandomLink';
 
 export default function Lobby(){
-    return(
-        <div className="home-page-lobby-container">
 
-            <div style={{ position: 'absolute', top: 0, left: 0, display: 'flex', alignItems: 'center', gap: '10px', padding: '10px', zIndex: 10 }}>
-                <img src="/logo1-recortado.png" alt="logo" style={{ height: 95, width: 95 }} />
-             </div>
-            <div className="top-right-lobby-buttons">
-                {/*<Link to="/register">
-                    <button className="button-register">📃Register</button>
-                </Link>
-                <Link to="/login">
-                    <button className="button-login">💻Login</button>
-                </Link>
-                */}
-                <Link to="/logout">
-                    <button className="button-logOut"> ➡️🚪Log Out</button>
-                </Link>
-                <Link to="/profile">
-                    <button className="button-logOut"> 👤Profile</button>
-                </Link>
-            </div>
+    const [isPrivate, setisPrivate] = useState(false);
+    const [player, setPlayer] = useState()
+    const [link,setlink] = useState("")
+    const [chat,setchat] = useState()
+    const jwt = tokenService.getLocalAccessToken();
+    const navigate = useNavigate();
 
-            <div className="hero-div-lobby">
-                <Link to="/CreateGame">
-                   <button className="button-crear">📑CREATE GAME</button>  
-                </Link>
-                 <Link to="/ListGames">
-                <button className="button-unirse">📥JOIN A GAME</button>   
-                </Link>
-            </div>
-            <div className="bottom-left-button">
-                <button className="button-ranking">🏆RANKING</button>
-             </div>
-        </div>
+
+    useEffect(() => {
+    const fetchPlayer = async () => {
+          try {
+            const loggedInUser = tokenService.getUser();
+          if (!loggedInUser || !loggedInUser.id) {
+            console.error("No se encontró el ID del usuario.");
+            return;
+        }
+            const response = await fetch(`/api/v1/users/${loggedInUser.id}`, {
+              method: "GET",
+              headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${jwt}`
+              }
+            });
+            console.log(response);
+            if (response.ok) {
+              const data = await response.json();
+              setPlayer(data);
+            } else {
+              console.error('Respuesta no OK:', response.status);
+              alert('Error al obtener la información del jugador.');
+            }
+          } catch (error) {
+            console.error('Hubo un problema con la petición fetch:', error);
+            alert('Error de red. No se pudo conectar con el servidor.');
+          }
+        };
+        fetchPlayer()
         
+
+        console.log(player)
+        
+
+  },[jwt])
+
+    async function handleSubmit() {
+    const jwt = tokenService.getLocalAccessToken();
+
+    try {
+        
+        const chatResponse = await fetch("/api/v1/chats", {
+            method: "POST",
+            headers: { 
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${jwt}` 
+            },
+            body: JSON.stringify({ message: "" }), 
+        });
+
+        if (!chatResponse.ok) {
+            
+            const errorData = await chatResponse.json();
+            alert(`Error al crear el chat: ${errorData.message}`);
+            return; 
+        }
+
+        const newChat = await chatResponse.json();
+        alert("Chat creado con éxito!");
+        console.log("Chat creado:", newChat);
+
+        setchat(newChat); 
+        const randomPart = generateRandomLink(16);
+        const fullLink = `https://saboteur.com/game/${randomPart}`;
+        setlink(link)
+
+        const gameRequest = {
+            gameStatus: "CREATED",
+            link: fullLink, 
+            maxPlayers: 3,
+            creator: player.username,
+            chat: newChat.id, 
+            private: false
+        };
+
+        console.log('Enviando solicitud de partida:', gameRequest);
+
+        const gameResponse = await fetch("/api/v1/games", {
+            method: "POST",
+            headers: { 
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${jwt}` 
+            },
+            body: JSON.stringify(gameRequest),
+        });
+
+        if (gameResponse.ok) {
+            const newGame = await gameResponse.json();
+            alert("¡Partida creada con éxito!");
+            console.log("Partida creada:", newGame);
+            navigate('/CreateGame', { state: { game: newGame } });
+        } else {
+            const errorData = await gameResponse.json();
+            alert(`Error al crear la partida: ${errorData.message}`);
+        }
+
+    } catch (error) {
+        console.error('Hubo un problema con la petición fetch:', error);
+        alert('Error de red. No se pudo conectar con el servidor.');
+    }
+}
+
+    return(
+        <>
+            <div className="home-page-lobby-container">
+
+                <div style={{ position: 'absolute', top: 0, left: 0, display: 'flex', alignItems: 'center', gap: '10px', padding: '10px', zIndex: 10 }}>
+                    <img src="/logo1-recortado.png" alt="logo" style={{ height: 95, width: 95 }} />
+                </div>
+                <div className="top-right-lobby-buttons">
+                    {/*<Link to="/register">
+                        <button className="button-register">📃Register</button>
+                    </Link>
+                    <Link to="/login">
+                        <button className="button-login">💻Login</button>
+                    </Link>
+                    */}
+                    <Link to="/logout">
+                        <button className="button-logOut"> ➡️🚪Log Out</button>
+                    </Link>
+                    <Link to="/profile">
+                        <button className="button-logOut"> 👤Profile</button>
+                    </Link>
+                </div>
+
+                <div className="hero-div-lobby">
+                        <button className="button-crear" onClick={handleSubmit}>📑CREATE GAME</button>
+                    <Link to="/ListGames">
+                        <button className="button-unirse">📥JOIN A GAME</button>
+                    </Link>
+                </div>
+                <div className="bottom-left-button">
+                    <button className="button-ranking">🏆RANKING</button>
+                </div>
+            </div>
+        </>
     );
 }
