cc375167db2a4d0c7524ca5ede87dfc3aaca6084
Diego <diegoreydiego@gmail.com>
2025-10-16 20:42:44 +0200

    full Achievement module (repository,restController,Service)

--- src/main/java/es/us/dp1/l4_04_24_25/saboteur/achievements/AchievementRepository.java
+++ src/main/java/es/us/dp1/l4_04_24_25/saboteur/achievements/AchievementRepository.java
@@ -1,27 +1,28 @@
 package es.us.dp1.l4_04_24_25.saboteur.achievements;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
-public interface AchievementRepository extends CrudRepository<Achievement,Integer>{
+public interface AchievementRepository extends CrudRepository<Achievement, Integer> {
 
-    /*
-    // Para obtener los logros de un jugador a partir de su id
+    @Override
+    Optional<Achievement> findById(Integer id);
+
+   
     @Query("SELECT a FROM Achievement a JOIN a.players p WHERE p.id = :playerId")
     List<Achievement> findAchievementsByPlayerId(@Param("playerId") Integer playerId);
 
-    // Para obtener los logros del admin
+
     @Query("SELECT a FROM Achievement a WHERE a.creator.id = :adminId")
     List<Achievement> findAchievementsByCreatorId(@Param("adminId") Integer adminId);
 
-    // Obtener los logros a partir del título (búsqueda exacta)
-    @Query("SELECT a FROM Achievement a WHERE a.tittle = :tittle")
-    List<Achievement> findAchievementsByTittle(@Param("tittle") String tittle);
-
-    // Contar el número de logros de un jugador
+    List<Achievement> findByTittle(String tittle);
+    
+    
     @Query("SELECT COUNT(a) FROM Achievement a JOIN a.players p WHERE p.id = :playerId")
     Integer countAchievementsByPlayerId(@Param("playerId") Integer playerId);
-    */
-    
-
-    
 }
---
+++ src/main/java/es/us/dp1/l4_04_24_25/saboteur/achievements/AchievementService.java
@@ -0,0 +1,54 @@
+package es.us.dp1.l4_04_24_25.saboteur.achievements;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import es.us.dp1.l4_04_24_25.saboteur.exceptions.ResourceNotFoundException;
+import jakarta.validation.Valid;
+
+@Service
+public class AchievementService {
+
+    private final AchievementRepository achievementRepository;
+
+    @Autowired
+    public AchievementService(AchievementRepository achievementRepository) {
+        this.achievementRepository = achievementRepository;
+    }
+
+
+    @Transactional
+    public Achievement saveAchievement(Achievement achievement) throws DataAccessException {
+        achievementRepository.save(achievement);
+        return achievement;
+    }
+
+    @Transactional(readOnly = true)
+    public Achievement findAchievement(Integer id) {
+        return achievementRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Achievement", "id", id));
+    }
+
+    @Transactional(readOnly = true)
+    public Iterable<Achievement> findAll() {
+        return achievementRepository.findAll();
+    }
+
+    @Transactional
+    public Achievement updateAchievement(@Valid Achievement achievement, Integer idToUpdate) {
+        Achievement toUpdate = findAchievement(idToUpdate);
+        BeanUtils.copyProperties(achievement, toUpdate, "id");
+        achievementRepository.save(toUpdate);
+        return toUpdate;
+    }
+
+    @Transactional
+    public void deleteAchievement(Integer id) {
+        Achievement toDelete = findAchievement(id);
+        achievementRepository.delete(toDelete);
+    }
+
+}
