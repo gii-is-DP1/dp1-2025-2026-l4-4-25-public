fa15b432aa7d3d13b8f96191cd8888efe2965cef
wrg8176 <lorvalrom@alum.us.es>
2025-10-22 18:09:53 +0200

    Serializer and Deserializer for Achievement and added a GET byTittle to Achievement

--- src/main/java/es/us/dp1/l4_04_24_25/saboteur/achievements/AchievementRestController.java
+++ src/main/java/es/us/dp1/l4_04_24_25/saboteur/achievements/AchievementRestController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -79,4 +80,9 @@ public class AchievementRestController {
         achievementService.deleteAchievement(id);
         return new ResponseEntity<>(new MessageResponse("Achievement deleted!"), HttpStatus.OK);
     }
-}
+
+    @GetMapping("byTittle")
+        public ResponseEntity<Achievement> findByTittle(@RequestParam String tittle){
+            return new ResponseEntity<>(achievementService.findByTittle(tittle), HttpStatus.OK);
+        }
+    }
