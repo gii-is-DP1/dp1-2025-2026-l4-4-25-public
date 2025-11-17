ed5bfdad47d016445937e4b05c2690111bfa88b3
Carlosbox2k <carborort@alum.us.es>
2025-11-17 18:01:26 +0100

    Added to BaseEntity @EqualsAndHashCode(of = "id")

--- src/main/java/es/us/dp1/l4_04_24_25/saboteur/baseEntities/BaseEntity.java
+++ src/main/java/es/us/dp1/l4_04_24_25/saboteur/baseEntities/BaseEntity.java
@@ -22,6 +22,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
 import jakarta.persistence.SequenceGenerator;
+import lombok.EqualsAndHashCode;
 
 /**
  * Simple JavaBean domain object with an id property. Used as a base class for objects
@@ -30,6 +31,7 @@ import jakarta.persistence.SequenceGenerator;
  * @author Ken Krebs
  * @author Juergen Hoeller
  */
+@EqualsAndHashCode(of = "id")
 @MappedSuperclass
 public class BaseEntity {
 
