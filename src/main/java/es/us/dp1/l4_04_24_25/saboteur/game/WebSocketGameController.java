05126da49b3cba9f0d07765bd03472f121e727cf
Carlosbox2k <carborort@alum.us.es>
2026-01-14 12:27:27 +0100

    Remove commented code and reorder imports in Game and WebSocketGameController classes

--- src/main/java/es/us/dp1/l4_04_24_25/saboteur/game/WebSocketGameController.java
+++ src/main/java/es/us/dp1/l4_04_24_25/saboteur/game/WebSocketGameController.java
@@ -1,12 +1,13 @@
 package es.us.dp1.l4_04_24_25.saboteur.game;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
-import es.us.dp1.l4_04_24_25.saboteur.round.RoundService;
+
 import es.us.dp1.l4_04_24_25.saboteur.player.PlayerService;
+import es.us.dp1.l4_04_24_25.saboteur.round.RoundService;
 
 @Controller
 public class WebSocketGameController {
@@ -31,9 +32,6 @@ public class WebSocketGameController {
     @MessageMapping("/games/start/{gameId}")
     public void startGame(@DestinationVariable Integer gameId) {
         Game game = gameService.findGame(gameId);
-        // Solo notifica a los clientes; el PATCH desde React ya cambió el estado
-
-        // Envía notificación a todos los clientes suscritos al topic
         messagingTemplate.convertAndSend("/topic/game/" + gameId, game);
     }
 }
